d907679ddd2d0bd6e28e6c6dbfa82104198f3300
junianto <junianto.palmco@aaico.com>
2023-12-13 21:38:25 +0700

    fix: add login and registration

--- src/features/login/containers/Login.container.tsx
+++ src/features/login/containers/Login.container.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import * as React from "react";
 import clsx from "clsx";
 import { getDictionaries } from "../i18";
@@ -7,6 +7,7 @@ import { libreBaskerville } from "@/core/fonts";
 import { LoginFormLogin } from "../fragments/login_form";
 import { GoogleLoginFormLogin } from "../fragments/google_login_form";
 import { FormProvider, useForm } from "react-hook-form";
+import { IntroductionLogin } from "../fragments/introduction/Introduction.login";
 
 export const LoginContainer = () => {
   const dictionaries = getDictionaries("en");
@@ -15,84 +16,94 @@ export const LoginContainer = () => {
     <FormProvider {...methods}>
       <div
         className={clsx(
-          "grid grid-cols-1 place-content-center place-items-center gap-[1.5rem]",
-          "w-full h-[100vh]",
-          "px-[1.5rem]"
+          "grid grid-cols-1 lg:grid-cols-2 place-content-center place-items-center gap-[1.5rem]",
+          "w-full h-[100vh]"
         )}
       >
-        <h1
-          className={clsx(
-            "text-[34px] font-bold text-[#232931]",
-            libreBaskerville.className
-          )}
-        >
-          {dictionaries.brand.name}
-        </h1>
-
         <div
           className={clsx(
-            "grid grid-cols-1 place-content-start place-items-start gap-[1.5rem]",
-            "max-w-[460px] w-full",
-            "pt-[2rem] pb-[1.5rem] px-[30px]",
-            "bg-white",
-            "rounded-[0.5rem]"
+            "grid grid-cols-1 place-content-center place-items-center gap-[1.5rem]",
+            "w-full h-[100vh]",
+            "px-[1.5rem]"
           )}
-          style={{ boxShadow: "0px 1px 29px 0px rgba(0, 67, 71, 0.10)" }}
         >
-          <div
+          <h1
             className={clsx(
-              "grid grid-cols-1 items-start content-start justify-center justify-items-center gap-[1rem]",
-              "w-full"
+              "text-[34px] font-bold text-[#232931]",
+              libreBaskerville.className
             )}
           >
-            <h2
-              className={clsx(
-                "text-[1.5rem] font-bold text-[#232931] text-center"
-              )}
-            >
-              {dictionaries.form.name}
-            </h2>
-            <p
-              className={clsx(
-                "text-[0.875rem] font-normal text-[#404852] text-center"
-              )}
-            >
-              {dictionaries.form.description}
-            </p>
-          </div>
+            {dictionaries.brand.name}
+          </h1>
 
           <div
             className={clsx(
               "grid grid-cols-1 place-content-start place-items-start gap-[1.5rem]",
-              "w-full"
+              "max-w-[460px] w-full",
+              "pt-[2rem] pb-[1.5rem] px-[30px]",
+              "bg-white",
+              "rounded-[0.5rem]"
             )}
+            style={{ boxShadow: "0px 1px 29px 0px rgba(0, 67, 71, 0.10)" }}
           >
-            <LoginFormLogin />
             <div
               className={clsx(
-                "grid grid-cols-[1fr_auto_1fr] items-center content-center justify-start justify-item-start gap-[0.75rem]",
+                "grid grid-cols-1 items-start content-start justify-center justify-items-center gap-[1rem]",
                 "w-full"
               )}
             >
-              <div className={clsx("w-full h-[1px]", "bg-[#9AA8B8]")} />
-              <p className={clsx("text-[1rem] font-normal text-[#9AA8B8]")}>
-                {"or"}
+              <h2
+                className={clsx(
+                  "text-[1.5rem] font-bold text-[#232931] text-center"
+                )}
+              >
+                {dictionaries.form.name}
+              </h2>
+              <p
+                className={clsx(
+                  "text-[0.875rem] font-normal text-[#404852] text-center"
+                )}
+              >
+                {dictionaries.form.description}
               </p>
-              <div className={clsx("w-full h-[1px]", "bg-[#9AA8B8]")} />
             </div>
-            <GoogleLoginFormLogin />
+
+            <div
+              className={clsx(
+                "grid grid-cols-1 place-content-start place-items-start gap-[1.5rem]",
+                "w-full"
+              )}
+            >
+              <LoginFormLogin />
+              <div
+                className={clsx(
+                  "grid grid-cols-[1fr_auto_1fr] items-center content-center justify-start justify-item-start gap-[0.75rem]",
+                  "w-full"
+                )}
+              >
+                <div className={clsx("w-full h-[1px]", "bg-[#9AA8B8]")} />
+                <p className={clsx("text-[1rem] font-normal text-[#9AA8B8]")}>
+                  {"or"}
+                </p>
+                <div className={clsx("w-full h-[1px]", "bg-[#9AA8B8]")} />
+              </div>
+              <GoogleLoginFormLogin />
+            </div>
           </div>
+
+          <p className={clsx("text-[1rem] font-normal text-[#404852]")}>
+            {dictionaries.form.actions.dont_have_account.description}
+            <Link
+              href={dictionaries.form.actions.dont_have_account.actions.link}
+              className={clsx("text-[1rem] font-semibold text-[#232931]")}
+            >
+              {dictionaries.form.actions.dont_have_account.actions.name}
+            </Link>
+          </p>
         </div>
 
-        <p className={clsx("text-[1rem] font-normal text-[#404852]")}>
-          {dictionaries.form.actions.dont_have_account.description}
-          <Link
-            href={dictionaries.form.actions.dont_have_account.actions.link}
-            className={clsx("text-[1rem] font-semibold text-[#232931]")}
-          >
-            {dictionaries.form.actions.dont_have_account.actions.name}
-          </Link>
-        </p>
+        {/* right view */}
+        <IntroductionLogin />
       </div>
     </FormProvider>
   );
---
+++ src/features/registration/fragments/introduction/Introduction.registration.tsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+import clsx from "clsx";
+import { getDictionaries } from "../../i18";
+
+export const IntroductionRegistration = () => {
+  const dictionaries = getDictionaries("en");
+  return (
+    <div
+      className={clsx(
+        "hidden lg:grid grid-cols-1 place-content-center place-items-center gap-[4rem]",
+        "w-full h-full",
+        "bg-[#EBF3FA]",
+        "px-[1.5rem]"
+      )}
+    >
+      <img
+        src={"/images/login/nexus.png"}
+        className={clsx("w-[580px] h-[316px]")}
+      />
+      <div
+        className={clsx(
+          "grid grid-cols-1 place-content-center place-items-center gap-[4rem]",
+          "w-full h-full"
+        )}
+      >
+        <div className={clsx("max-w-[600px] w-full")}>
+          <p
+            className={clsx(
+              "text-[1.125rem] font-normal text-[#404852] text-center"
+            )}
+            dangerouslySetInnerHTML={{
+              __html: dictionaries.introduction.message,
+            }}
+          />
+        </div>
+
+        <div
+          className={clsx(
+            "grid grid-cols-1 items-start content-start justify-center justify-items-center gap-[1.5rem]",
+            "w-full"
+          )}
+        >
+          <p
+            className={clsx(
+              "text-[1.125rem] font-medium text-center text-[#697584]"
+            )}
+          >
+            {dictionaries.introduction.sponsor.title}
+          </p>
+          <div
+            className={clsx(
+              "grid grid-flow-col place-content-center place-items-center gap-[1.5rem]",
+              "w-full"
+            )}
+          >
+            <img
+              src={"/images/login/kumparan.png"}
+              className={clsx("w-[150px] h-[2.25rem]")}
+            />
+            <img
+              src={"/images/login/tech_in_asia.png"}
+              className={clsx("w-[180px] h-[2rem]")}
+            />
+            <img
+              src={"/images/login/cosmopolitan.png"}
+              className={clsx("w-[130px] h-[1.5rem]")}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
