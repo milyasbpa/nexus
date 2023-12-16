9ce9d5d3c977b76c8678aec3053d02eeb95b8787
junianto <junianto.palmco@aaico.com>
2023-12-16 10:20:29 +0700

    fix: fonts

--- src/core/fonts/fonts.ts
+++ src/core/fonts/fonts.ts
@@ -3,8 +3,10 @@ import { Libre_Baskerville, Plus_Jakarta_Sans } from "next/font/google";
 export const libreBaskerville = Libre_Baskerville({
   subsets: ["latin"],
   weight: ["400", "700"],
+  variable: '--font-libre-baskerville'
 });
 
 export const plusJakartaSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
+  variable: '--font-plus-jakarta-sans'
 });
