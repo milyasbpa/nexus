fb745f47a4a638c6b81cb453f529cdb763f8dc20
junianto <junianto.palmco@aaico.com>
2023-12-16 18:15:58 +0700

    fix: login

--- src/features/login/fragments/login_form/LoginForm.login.tsx
+++ src/features/login/fragments/login_form/LoginForm.login.tsx
@@ -10,20 +10,41 @@ import { LoginForm } from "../../react_hook_form/keys";
 import { useLoginSignInWithEmailAndPasswordFirebase } from "../../react_query/hooks/useSignInWithEmailAndPasswordFirebase.login";
 import { useLoginGetRememberMeStorage } from "../../react_query/hooks/useGetRememberMeStorage.login";
 import { useLoginSetRememberMeStorage } from "../../react_query/hooks/useSetRememberMeStorage.login";
+import { Regex } from "@/core/utils/validation";
 
 export const LoginFormLogin = () => {
   const dictionaries = getDictionaries("en");
-  const { setValue, watch } = useFormContext<LoginForm>();
+  const {
+    setValue,
+    watch,
+    setError,
+    clearErrors,
+    formState: { errors },
+  } = useFormContext<LoginForm>();
   useLoginGetRememberMeStorage();
   const { mutate: signInWithEmailAndPasswordFirebase } =
     useLoginSignInWithEmailAndPasswordFirebase();
   const { mutate: setRememberMeStorage } = useLoginSetRememberMeStorage();
 
   const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!Regex.email.test(e.currentTarget.value)) {
+      setError(e.currentTarget.name, {
+        message: dictionaries.form.errors.invalid_email.message,
+      });
+    } else {
+      clearErrors(e.currentTarget.name);
+    }
     setValue(e.currentTarget.name, e.currentTarget.value);
   };
 
   const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!Regex.password.test(e.currentTarget.value)) {
+      setError(e.currentTarget.name, {
+        message: dictionaries.form.errors.invalid_password_format.message,
+      });
+    } else {
+      clearErrors(e.currentTarget.name);
+    }
     setValue(e.currentTarget.name, e.currentTarget.value);
   };
 
@@ -42,7 +63,10 @@ export const LoginFormLogin = () => {
 
   const isLoginDisabled =
     !watch(dictionaries.form.email.name)?.length ||
-    !watch(dictionaries.form.password.name)?.length;
+    !watch(dictionaries.form.password.name)?.length ||
+    !!Object.keys(errors).length;
+
+  console.log(errors, "ini errors");
   return (
     <div
       className={clsx(
@@ -50,20 +74,53 @@ export const LoginFormLogin = () => {
         "w-full"
       )}
     >
-      <Textfield
-        name={dictionaries.form.email.name}
-        label={dictionaries.form.email.label}
-        value={watch(dictionaries.form.email.name)}
-        placeholder={dictionaries.form.email.placeholder}
-        onChange={handleChangeEmail}
-      />
-      <Passwordfield
-        name={dictionaries.form.password.name}
-        label={dictionaries.form.password.label}
-        value={watch(dictionaries.form.password.name)}
-        placeholder={dictionaries.form.password.placeholder}
-        onChange={handleChangePassword}
-      />
+      <div
+        className={clsx(
+          "grid grid-cols-1 place-content-start place-items-start gap-[0.25rem]",
+          "w-full"
+        )}
+      >
+        <Textfield
+          name={dictionaries.form.email.name}
+          label={dictionaries.form.email.label}
+          value={watch(dictionaries.form.email.name)}
+          placeholder={dictionaries.form.email.placeholder}
+          onChange={handleChangeEmail}
+        />
+        {errors[dictionaries.form.email.name] && (
+          <p
+            className={clsx(
+              "text-[0.625rem] text-[#FC5959] font-normal font-plusJakartaSans"
+            )}
+          >
+            {String(errors[dictionaries.form.email.name]?.message)}
+          </p>
+        )}
+      </div>
+
+      <div
+        className={clsx(
+          "grid grid-cols-1 place-content-start place-items-start gap-[0.25rem]",
+          "w-full"
+        )}
+      >
+        <Passwordfield
+          name={dictionaries.form.password.name}
+          label={dictionaries.form.password.label}
+          value={watch(dictionaries.form.password.name)}
+          placeholder={dictionaries.form.password.placeholder}
+          onChange={handleChangePassword}
+        />
+        {errors[dictionaries.form.password.name] && (
+          <p
+            className={clsx(
+              "text-[0.625rem] text-[#FC5959] font-normal font-plusJakartaSans"
+            )}
+          >
+            {String(errors[dictionaries.form.password.name]?.message)}
+          </p>
+        )}
+      </div>
       <Checkbox
         label={dictionaries.form.remember_me.label}
         name={dictionaries.form.remember_me.name}
