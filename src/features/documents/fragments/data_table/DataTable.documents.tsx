baa0ea10baf00d21ec95682ebd507d979c9fc40c
junianto <junianto.palmco@aaico.com>
2023-12-16 18:30:39 +0700

    fix: empty message

--- src/features/documents/fragments/data_table/DataTable.documents.tsx
+++ src/features/documents/fragments/data_table/DataTable.documents.tsx
@@ -65,7 +65,10 @@ export const DataTableDocuments = () => {
     debugTable: true,
   });
 
-  if (false) {
+  if (
+    !watch(dictionaries.data_table.name).length &&
+    !watch(dictionaries.data_table.search.input.name).length
+  ) {
     return (
       <div
         className={clsx(
@@ -94,6 +97,44 @@ export const DataTableDocuments = () => {
           />
           <p className={clsx("text-[#697584] text-[1rem] font-normal")}>
             {dictionaries.data_table.initial.message}
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  if (
+    !watch(dictionaries.data_table.name).length &&
+    !!watch(dictionaries.data_table.search.input.name).length
+  ) {
+    return (
+      <div
+        className={clsx(
+          "grid grid-cols-1 place-content-start place-items-start gap-[1rem]",
+          "w-full",
+          "rounded-[0.25rem]",
+          "overflow-hidden"
+        )}
+      >
+        <table className={clsx("wide-spacing", "w-full")}>
+          <TableHeadDocuments table={table} />
+        </table>
+        <div
+          className={clsx(
+            "grid grid-cols-1 place-content-center place-items-center gap-[1rem]",
+            "h-[400px] w-full",
+            "bg-[white]"
+          )}
+        >
+          <Image
+            src={"/icons/documents/empty_file.svg"}
+            alt={"empty-data"}
+            width={60}
+            height={60}
+            className={clsx("w-[60px] h-[60px]")}
+          />
+          <p className={clsx("text-[#697584] text-[1rem] font-normal")}>
+            {dictionaries.data_table.empty.message}
           </p>
         </div>
       </div>
